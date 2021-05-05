a8fd518d18c0c57b419dbb9624e607a0e22994e8
Akshat Garg <akshatarungarg78@gmail.com>
2021-05-05 15:38:11 +0530

    added functions for getting investments from firestore

--- client/src/components/Pages/Portfolio.tsx
+++ client/src/components/Pages/Portfolio.tsx
@@ -1,30 +1,52 @@
- import {useEffect} from 'react'
+ import {useEffect, useState} from 'react'
 import { useHistory } from 'react-router'
 import { useAuth } from '../../context/AuthContextProvider'
-import { getPortfolioData } from '../../util/portfolio'
+import { getCryptoCurrencies, getMutualFunds, getPortfolioData, getStocks } from '../../util/portfolio'
+
+export interface PortfolioPageProps{
+    name: any;
+    createdAt: any;
+    totalValue: any;
+    stocks: string[];
+    cryptoCurrencies: string[];
+    mutualFunds: string[];
+  }
 
  const PortfolioPage: React.FC<any> = (props) => {
   const id = props?.match?.params?.id
   const {user} = useAuth()
   const history = useHistory()
-
+  const [data, setData] = useState<PortfolioPageProps | null>(null) 
+  const [loading, setLoading] = useState<boolean>(false) 
   useEffect(()=> {
     // create a function to take index and email of user and return the portfolio info.
     const callData = async  () => {
       try{
-       await getPortfolioData(id)
+       setLoading(true)
+       const d:PortfolioPageProps = await getPortfolioData(id)
+       console.log(d)
+       
+       await Promise.all([getStocks(d.stocks), getMutualFunds(d.mutualFunds), getCryptoCurrencies(d.cryptoCurrencies)]).then((data) => {
+          console.log(data)
+       })
+       setData(d)
       }
       catch(e){
         console.log(e)
         history.push('/')
+      }finally{
+        setLoading(false)
       }
     }
     callData()
   }, [id, history])
-
+  if(loading || !data){
+    return <h4>Loading...</h4>
+  }
   return (
     <div>
-      <h4>Portfolio Number {id} of user {user.displayName}</h4>
+      <h4>Portfolio Number {data.name} of user {user.displayName}</h4>
+      <h4>Total value : {data.totalValue}</h4>
     </div>
   );
 }
--- client/src/util/portfolio/index.ts
+++ client/src/util/portfolio/index.ts
@@ -1,12 +1,72 @@
 import {database} from '../../config/firebase'
+import firebase from 'firebase/app'
+import {asyncForEach} from '../custom'
 
 export const getPortfolioData = async (id:string) => {
-  const db = await database.collection('portfolios').doc(id).get()
-  const portfolio = db.data()
-  if(!portfolio){
-    // throw error
-    throw new Error("Portfolio not found")
+  try{
+    const db = await database.collection('portfolios').doc(id).get()
+    const portfolio = db.data()
+    if(!portfolio){
+      // throw error
+      throw new Error("Portfolio not found")
+    }
+    
+    // console.log(portfolio.stocks[0].id)
+    const stocks: Array<string> = portfolio.stocks.map((s:any) => s.id)
+    const cryptoCurrencies: Array<string> = portfolio.cryptoCurrencies.map((s:any) => s.id)
+    const mutualFunds: Array<string> = portfolio.mutualFunds.map((s:any) => s.id)
+  
+    console.log(portfolio)
+    const {name, createdAt, totalValue} = portfolio
+    return {
+      name,
+      createdAt, 
+      totalValue,
+      stocks,
+      cryptoCurrencies,
+      mutualFunds
+    }
   }
-  console.log(portfolio)
+  catch(e){
+    console.log(e)
+    throw e
+  }
   // extract stocks , cryptos , mutualFunds by creating three different functions which can be called, later 
+}
+
+
+export const getStocks = async (stockIds : Array<string>) => {
+  const stocks:any = []
+  const callback = async (id:string) => {
+    const d = await database.collection('stocks').doc(id).get()
+    const data = d.data()
+    stocks.push(data)
+  }
+  await asyncForEach(stockIds,callback)
+  return stocks
+}
+
+
+export const getMutualFunds = async (mutualFundIds : Array<string>) => {
+  const mutualFunds:any = []
+  const callback = async (id:string) => {
+    const d = await database.collection('mutualFunds').doc(id).get()
+    const data = d.data()
+    mutualFunds.push(data)
+  }
+  await asyncForEach(mutualFundIds,callback)
+  return mutualFunds
+}
+
+
+export const getCryptoCurrencies = async (cryptoCurrenciesIds : Array<string>) => {
+  // const refs = cryptoCurrenciesIds.map((id) => `stocks/${id}`)
+  const cryptoCurrencies:any = []
+  const callback = async (id:string) => {
+    const d = await database.collection('cryptoCurrencies').doc(id).get()
+    const data = d.data()
+    cryptoCurrencies.push(data)
+  }
+  await asyncForEach(cryptoCurrenciesIds,callback)
+  return cryptoCurrencies
 }
