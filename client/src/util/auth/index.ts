c0276bf410f3dbf14965e0ab43db5eb869226c68
Akshat Garg <akshatarungarg78@gmail.com>
2021-05-08 16:00:49 +0530

    Auth class created with authentication methods

--- client/src/util/auth/index.ts
+++ client/src/util/auth/index.ts
@@ -1,55 +1,102 @@
 import firebase from 'firebase/app'
 import {auth, database} from  '../../config/firebase'
 
-const loginWithGoogle = (callback : ()=>void) => {
-  var provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({
-    prompt: 'select_account'
-  });
-  provider.addScope('profile');
-  provider.addScope('email');
- 
-  auth
-  .signInWithPopup(provider)
-  .then((result) => {
-    database.collection("users").where("email", "==", result.user?.email).get().then(data => {
-      // console.log(data.size)
-      if(data.size){
-        console.log("user exists in db")
-      }
-      else{
-        if(result.user){
-          const {displayName, email, photoURL} = result.user
-          register({displayName, email, photoURL})
+export class Auth{
+  login(callback : ()=>void){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.setCustomParameters({
+      prompt: 'select_account'
+    });
+    provider.addScope('profile');
+    provider.addScope('email');
+   
+    auth
+    .signInWithPopup(provider)
+    .then((result) => {
+      database.collection("users").where("email", "==", result.user?.email).get().then(data => {
+        // console.log(data.size)
+        if(data.size){
+          console.log("user exists in db")
         }
-      }
-    }).then(()=> callback())
-    // check if this email exists in database if not create a new user.
-    }).catch((error) => {
-      console.log(error)
-      throw error
+        else{
+          if(result.user){
+            const {displayName, email, photoURL} = result.user
+            this.register({displayName, email, photoURL})
+          }
+        }
+      }).then(()=> callback())
+      // check if this email exists in database if not create a new user.
+      }).catch((error) => {
+        console.log(error)
+        throw error
+      })
+  }
+  register({displayName, email,photoURL}:any){
+    database.collection("users").add({
+      name : displayName,
+      email : email,
+      displayPic : photoURL,
+      portfolios: []
+    }).then((doc) => {
+      console.log(doc)
     })
+  }
+  logout(){
+    auth.signOut().then(()=>{
+      console.log("signed out")
+    })
+  }
 }
 
-const register = ({displayName, email,photoURL}:any) => {
-  database.collection("users").add({
-    name : displayName,
-    email : email,
-    displayPic : photoURL,
-    portfolios: []
-  }).then((doc) => {
-    console.log(doc)
-  })
-}
+// const loginWithGoogle = (callback : ()=>void) => {
+//   var provider = new firebase.auth.GoogleAuthProvider();
+//   provider.setCustomParameters({
+//     prompt: 'select_account'
+//   });
+//   provider.addScope('profile');
+//   provider.addScope('email');
+ 
+//   auth
+//   .signInWithPopup(provider)
+//   .then((result) => {
+//     database.collection("users").where("email", "==", result.user?.email).get().then(data => {
+//       // console.log(data.size)
+//       if(data.size){
+//         console.log("user exists in db")
+//       }
+//       else{
+//         if(result.user){
+//           const {displayName, email, photoURL} = result.user
+//           register({displayName, email, photoURL})
+//         }
+//       }
+//     }).then(()=> callback())
+//     // check if this email exists in database if not create a new user.
+//     }).catch((error) => {
+//       console.log(error)
+//       throw error
+//     })
+// }
 
-const logout = () => {
-  auth.signOut().then(()=>{
-    console.log("signed out")
-  })
-}
+// const register = ({displayName, email,photoURL}:any) => {
+//   database.collection("users").add({
+//     name : displayName,
+//     email : email,
+//     displayPic : photoURL,
+//     portfolios: []
+//   }).then((doc) => {
+//     console.log(doc)
+//   })
+// }
 
-export {
-  loginWithGoogle,
-  register,
-  logout
-}
+// const logout = () => {
+//   auth.signOut().then(()=>{
+//     console.log("signed out")
+//   })
+// }
+
+// export {
+//   loginWithGoogle,
+//   register,
+//   logout
+// }
