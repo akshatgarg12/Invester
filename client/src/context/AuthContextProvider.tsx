1fc46657e49e2b6b7727c34b161bc852a4f6b6a8
Akshat Garg <akshatarungarg78@gmail.com>
2021-05-08 16:11:16 +0530

    abstracted currenUserState in auth class

--- client/src/context/AuthContextProvider.tsx
+++ client/src/context/AuthContextProvider.tsx
@@ -1,5 +1,6 @@
-import {createContext, useContext, useEffect, useState} from 'react'
-import {auth} from '../config/firebase'
+import {createContext, useContext, useEffect, useMemo, useState} from 'react'
+import { Auth } from '../util/auth'
+// import {auth} from '../config/firebase'
 
 const AuthContext: React.Context<any> = createContext(null)
 
@@ -8,20 +9,21 @@ export const useAuth = () => useContext(AuthContext)
 const AuthContextProvider: React.FC<any> = ({children}) => {
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState<any>(null)
-  
+  const auth = useMemo(()=> new Auth(), [])
   useEffect(()=>{
-    auth.onAuthStateChanged((u) => {
-      if(u){
-        const {displayName, email, photoURL} = u
-        setUser({displayName, email, photoURL})
-      }else{
-        setUser(null)
+    const callback = (u:any) => {
+        if(u){
+          const {displayName, email, photoURL} = u
+          setUser({displayName, email, photoURL})
+        }else{
+          setUser(null)
+        }
+        console.log(u);
+        setLoading(false)
+        // console.log(user)
       }
-      console.log(u);
-      setLoading(false)
-      // console.log(user)
-    })
-  }, [])
+      auth.currentUser(callback)  
+  },[auth])
 
   if(loading){
     return <h6>loading...</h6>
--- client/src/util/auth/index.ts
+++ client/src/util/auth/index.ts
@@ -46,57 +46,9 @@ export class Auth{
       console.log("signed out")
     })
   }
+  currentUser(callback :(u:any)=>void){
+    auth.onAuthStateChanged((u) => {
+      callback(u)
+    })
+  }
 }
-
-// const loginWithGoogle = (callback : ()=>void) => {
-//   var provider = new firebase.auth.GoogleAuthProvider();
-//   provider.setCustomParameters({
-//     prompt: 'select_account'
-//   });
-//   provider.addScope('profile');
-//   provider.addScope('email');
- 
-//   auth
-//   .signInWithPopup(provider)
-//   .then((result) => {
-//     database.collection("users").where("email", "==", result.user?.email).get().then(data => {
-//       // console.log(data.size)
-//       if(data.size){
-//         console.log("user exists in db")
-//       }
-//       else{
-//         if(result.user){
-//           const {displayName, email, photoURL} = result.user
-//           register({displayName, email, photoURL})
-//         }
-//       }
-//     }).then(()=> callback())
-//     // check if this email exists in database if not create a new user.
-//     }).catch((error) => {
-//       console.log(error)
-//       throw error
-//     })
-// }
-
-// const register = ({displayName, email,photoURL}:any) => {
-//   database.collection("users").add({
-//     name : displayName,
-//     email : email,
-//     displayPic : photoURL,
-//     portfolios: []
-//   }).then((doc) => {
-//     console.log(doc)
-//   })
-// }
-
-// const logout = () => {
-//   auth.signOut().then(()=>{
-//     console.log("signed out")
-//   })
-// }
-
-// export {
-//   loginWithGoogle,
-//   register,
-//   logout
-// }
