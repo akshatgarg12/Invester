37b1348cb5cafd9381edd494335b134a65aa63f8
Akshat Garg <akshatarungarg78@gmail.com>
2021-05-11 13:48:45 +0530

    created a util function to update only the required components

--- client/src/util/custom.ts
+++ client/src/util/custom.ts
@@ -1,6 +1,8 @@
 import {Portfolio} from './portfolio'
-import {Investment} from './investment'
+import {Investment, InvestmentType} from './investment'
 import { PortfolioPageProps } from '../components/Pages/Portfolio';
+import { InvestmentData } from '../components/Investments';
+
 export const asyncForEach = async (array:Array<any>, callback: any)  => {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
@@ -15,6 +17,28 @@ export const getPortfolioData = async (id:string) => {
     const {stocks, cryptoCurrencies, mutualFunds} = d
     const x:any = await investment.getAll(stocks, cryptoCurrencies, mutualFunds)
     return x
+  }catch(e){
+    console.log(e)
+    throw e
+  }
+}
+
+export const updatePortfolioData = async (toUpdateType : InvestmentType, initialData: InvestmentData, updateId : string, action : "ADD" | "DELETE") => {
+  try{
+    if(action === "ADD"){
+      const investment = new Investment()
+      const x:any = await investment.get([updateId], toUpdateType)
+      console.log(x)
+      initialData[toUpdateType].unshift(x[0])
+      return initialData
+    }
+    if(action === "DELETE"){
+      const updatedData = initialData[toUpdateType].filter((data) => data.id !== updateId)
+      return {
+        ...initialData,
+        [toUpdateType] : updatedData
+      }
+    }
   }catch(e){
     console.log(e)
     throw e
