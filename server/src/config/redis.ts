b836439d483bd616a51e068249d10f0bf575135d
Akshat Garg <akshatarungarg78@gmail.com>
2021-05-15 02:30:37 +0530

    changed redis config to connect to cloud hosted redis in prod

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,7 +1,19 @@
 import redis from 'redis'
-const REDIS_PORT:any = process.env.REDIS_URL || 6379;
+import dotenv from 'dotenv'
+dotenv.config()
+// heroku config
+// const REDIS_PORT:any = process.env.REDIS_URL || 6379;
 
-const client = redis.createClient(REDIS_PORT, {no_ready_check: true});
+const __prod__ = process.env.NODE_ENV === "production"
+// redis cloud service config
+const REDIS_PORT:any = __prod__ && process.env.REDIS_PORT
+const REDIS_HOST:any = __prod__ && process.env.REDIS_HOST
+const REDIS_PASSWORD:any = __prod__ && process.env.REDIS_PASSWORD
+const client = redis.createClient({
+  host:REDIS_HOST,
+  port:REDIS_PORT,
+  password : REDIS_PASSWORD
+});
 
 client.on("error", function(error) {
   console.error(error);
