0778d3ec96633f36cb571d0dde4e3a7616d57ce8
Akshat Garg <akshatarungarg78@gmail.com>
2021-05-15 01:37:12 +0530

    changed prod url

--- client/src/constant.ts
+++ client/src/constant.ts
@@ -1,5 +1,5 @@
 const SERVER_DEV_URL = "http://localhost:8080"
-const SERVER_PROD_URL = "http://localhost:8080"
+const SERVER_PROD_URL = "https://invester-api.herokuapp.com"
 export const SERVER_URL = process.env.NODE_ENV === "development" ? SERVER_DEV_URL : SERVER_PROD_URL
 export const getCurrentStocksPriceURL = SERVER_URL + '/stocks'
 export const getCurrentCryptoPriceURL = SERVER_URL + '/crypto'
