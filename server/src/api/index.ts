658fa7c22b971c39f1a60c29889ba6c41bf7e8ac
Akshat Garg <akshatarungarg78@gmail.com>
2021-05-22 23:05:25 +0530

    api for crypto-suggestions created at server using fuse js

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -7,6 +7,7 @@ const router = Router()
 
 router.post('/stocks', StocksController.getPrices)
 router.post('/crypto', CryptoController.getPrices)
+router.get('/crypto/search', CryptoController.find)
 router.post('/mutualFunds', MutualFundsController.getPrices)
 router.get('/mutualFunds/search', MutualFundsController.find)
 router.get('/currency', CurrencyController.getRates)
